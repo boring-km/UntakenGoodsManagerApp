b888dde3dcfffb3d79dfca677ce307517ea1134a
boring-km <kms0644804@naver.com>
2021-03-14 16:19:19 +0900

    [JKM] 상품 목록 조회하기 위한 화면을 구현한다

--- app/src/main/java/com/emart24/ProductListActivity.kt
+++ app/src/main/java/com/emart24/ProductListActivity.kt
@@ -1,11 +1,56 @@
 package com.emart24
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.appcompat.app.AppCompatActivity
+import com.emart24.adapter.ProductListAdapter
+import com.emart24.model.UnTakenGoods
+import kotlinx.android.synthetic.main.activity_list.*
+import java.util.*
 
 class ProductListActivity: AppCompatActivity() {
+
+    private lateinit var adapter: ProductListAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list)
+
+        // TODO Firebase에서 데이터 가져오기
+        val resultList = Array<UnTakenGoods>(5) { i ->
+            UnTakenGoods("테스트 $i", "1234", "qr", "21년 3월 14일", false)
+        }
+        resultList[0] = UnTakenGoods("테스트 0", "1234", "qr", "21년 3월 14일", false)
+        resultList[1] = UnTakenGoods("테스트 1", "1111", "qr", "21년 3월 14일", false)
+        resultList[2] = UnTakenGoods("테스트 2", "2222", "qr", "21년 3월 14일", false)
+        resultList[3] = UnTakenGoods("테스트 3", "8887", "qr", "21년 3월 14일", false)
+
+        adapter = ProductListAdapter()
+        ProductListView.adapter = adapter
+
+        adapter.addItems(resultList)
+        adapter.notifyDataSetChanged()
+
+        editPhoneNumberView.addTextChangedListener(object: TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                val filterText = s.toString()
+                val length = filterText.length
+                adapter.clear()
+                Arrays.stream(resultList).filter { item -> item.phone.substring(0, length) == filterText }.forEach { item ->
+                    adapter.addItem(item)
+                }
+                adapter.notifyDataSetChanged()
+            }
+
+        })
     }
 }
---
+++ app/src/main/java/com/emart24/adapter/ProductListAdapter.kt
@@ -0,0 +1,103 @@
+package com.emart24.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.Filter
+import android.widget.Filterable
+import android.widget.TextView
+import com.emart24.R
+import com.emart24.model.UnTakenGoods
+
+class ProductListAdapter: BaseAdapter(), Filterable {
+
+    private val list = ArrayList<UnTakenGoods>()
+    private var filteredList: ArrayList<UnTakenGoods> = list
+    private var listFilter: Filter? = null
+
+    override fun getCount(): Int {
+        return filteredList.size
+    }
+
+    override fun getItem(position: Int): Any {
+        return filteredList[position]
+    }
+
+    override fun getItemId(position: Int): Long {
+        return position.toLong()
+    }
+
+
+    override fun getView(position: Int, view: View?, viewGroup: ViewGroup): View {
+        val context = viewGroup.context
+        var convertView: View? = view
+        if (convertView == null) {
+            val inflater: LayoutInflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+            convertView = inflater.inflate(R.layout.product_list_item, viewGroup, false)
+        }
+        val dateView = convertView!!.findViewById<TextView>(R.id.DateTextView)
+        val nameView = convertView.findViewById<TextView>(R.id.ProductNameTextView)
+        val acceptView = convertView.findViewById<TextView>(R.id.IsAcceptTextView)
+
+        val item = filteredList[position]
+        dateView.text = item.dateTime
+        nameView.text = item.name
+        if (item.accept) {
+            acceptView.text = "수령"
+        } else {
+            acceptView.text = "미수령"
+        }
+        return convertView
+    }
+
+    fun addItems(items: Array<UnTakenGoods>) {
+        filteredList.addAll(items)
+    }
+
+    fun addItem(item: UnTakenGoods) {
+        filteredList.add(item)
+    }
+
+    fun clear() {
+        filteredList.clear()
+    }
+
+    override fun getFilter(): Filter? {
+        if (listFilter == null) {
+            listFilter = ListFilter()
+        }
+        return listFilter
+    }
+
+    inner class ListFilter : Filter() {
+        override fun performFiltering(constraint: CharSequence): FilterResults {
+            val results = FilterResults()
+            if (constraint.isEmpty()) {
+                results.values = list
+                results.count = list.size
+            } else {
+                val itemList: ArrayList<UnTakenGoods> = ArrayList()
+                for (item in list) {
+                    if (item.phone.contains(constraint.toString())) {
+                        itemList.add(item)
+                    }
+                }
+                results.values = itemList
+                results.count = itemList.size
+            }
+            return results
+        }
+
+        override fun publishResults(charSequence: CharSequence, results: FilterResults) {
+            @Suppress("UNCHECKED_CAST")
+            filteredList = results.values as ArrayList<UnTakenGoods>
+            if (results.count > 0) {
+                notifyDataSetChanged()
+            } else {
+                notifyDataSetInvalidated()
+            }
+        }
+    }
+}
