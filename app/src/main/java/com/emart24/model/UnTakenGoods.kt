055780fbc0ead8c9915382f5a9ffe5e5a1b4a9e5
boring-km <kms0644804@naver.com>
2021-03-18 00:17:11 +0900

    fixup! [JKM] 상품 리스트 화면 개선하고 리스트 클릭 시 상세 화면이 나오게 한다

--- app/src/main/java/com/emart24/model/UnTakenGoods.kt
+++ app/src/main/java/com/emart24/model/UnTakenGoods.kt
@@ -1,11 +1,23 @@
 package com.emart24.model
 
-class UnTakenGoods() {
+import android.os.Parcel
+import android.os.Parcelable
+
+class UnTakenGoods(): Parcelable {
+
     lateinit var name: String
     lateinit var phone: String
     lateinit var qrcode: String
     lateinit var dateTime: String
     var accept: Boolean = false
+
+    constructor(parcel: Parcel) : this() {
+        name = parcel.readString()!!
+        phone = parcel.readString()!!
+        qrcode = parcel.readString()!!
+        dateTime = parcel.readString()!!
+        accept = parcel.readByte() != 0.toByte()
+    }
 
     constructor(name: String, phone: String, qrcode: String, dateTime: String, accept: Boolean) : this() {
         this.name = name
@@ -13,5 +25,27 @@ class UnTakenGoods() {
         this.qrcode = qrcode
         this.dateTime = dateTime
         this.accept = accept
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(name)
+        parcel.writeString(phone)
+        parcel.writeString(qrcode)
+        parcel.writeString(dateTime)
+        parcel.writeByte(if (accept) 1 else 0)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<UnTakenGoods> {
+        override fun createFromParcel(parcel: Parcel): UnTakenGoods {
+            return UnTakenGoods(parcel)
+        }
+
+        override fun newArray(size: Int): Array<UnTakenGoods?> {
+            return arrayOfNulls(size)
+        }
     }
 }
